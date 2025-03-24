7199d463db9931cc8946b80fa2f23c05e3e88e98
DenkiGUy <adarsh.praveen@smail.astate.edu>
2025-03-24 15:53:59 -0600

    hangman.py

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
-word_list = ["algorithms", "functions", "structures", "input", "output", "programming", "project", "colors", "animals", "food",
+import random
+class TriadHangman:
+    word_list = ["algorithms", "functions", "structures", "input", "output", "programming", "project", "colors", "animals", "food",
     "water", "mcdonalds", "pizza", "snickers", "card", "watch", "doctors", "medicine", "machine", "cars",
     "university", "tools", "messages", "preview", "music", "netflix", "mail", "settings", "calendar", "september",
     "weather", "files", "history", "classes", "view", "representation", "dictionary", "narrow", "simplify", "element",
@@ -11,9 +13,6 @@ word_list = ["algorithms", "functions", "structures", "input", "output", "progra
     "motorcycle", "handlebars", "kickstand", "helmet", "gears", "throttle", "chain", "sprocket", "fork", "shock absorber",
     "fairing", "footpegs", "fuel tank", "exhaust pipe", "rims", "tire pressure", "oil filter", "air filter", "coolant",
     "spark plugs", "piston", "camshaft", "crankshaft", "valves", "clutch lever", "brake pads", "disc brakes", "drum brakes",]
-
-import random
-class TriadHangman:
     def _init_(player, word_list, guessed_letters, total_attempts=6):
         player.word = random.choice(word_list)
         player.guessed_letters = set()

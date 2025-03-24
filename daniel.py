cdc94d690e133d059fb28b99035efb80f20e7e61
daniel 3 <91003420+Danx45@users.noreply.github.com>
2025-03-24 16:34:37 -0600

    Update daniel.py

--- daniel.py
+++ daniel.py
@@ -1,1 +1,9 @@
-
+while '_' in current_state:
+    for index, letter in enumerate(list(word)):
+        if letter == input_letter:
+            current_state[index] = input_letter
+    print(current_state)
+    if '_' in current_state: 
+        input_letter = input("idk: ")
+    else:
+        print(f'Good job! The word was "{word}"!')

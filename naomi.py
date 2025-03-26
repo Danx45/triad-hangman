5690cbda2f57e89a14ab40e3d7c9cd54a532e958
judithnao <judithnaomi22@gmail.com>
2025-03-26 13:14:06 -0600

    Update naomi.py

--- naomi.py
+++ naomi.py
@@ -1,9 +1,10 @@
 while True:
     if guess not in player.word:
-        player.guessed_letters.add(guess)
-        player.attempts_left -= 1
-        print(f"Lives left: {player.attempts_left}")
-        print(f"Incorrect guessed letters: {player.guessed_letters}")
-        if player.attempts_left == 0:
-            print(f"You've lost! Correct word was '{player.word}'.")
-            break
+        player.guesses_letters.add(guess)
+        player.attemps_left -= 1
+        print("Lives left: {player.atemps_left}")
+        print("Incorrect guessed letters: {player.guessed_letters}")
+    if player.attemps_left == 0:
+        print(f"You've lost! Correct word was '{player.word}'.")
+    break
+    guess = input("Guess another letter: ")
